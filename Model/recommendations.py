348451466f2396c9532c3424cf670f8010615b4b
JuliaTsiryulik <93212101+JuliaTsiryulik@users.noreply.github.com>
2023-12-13 11:44:56 +0300

    Model baseline

---
+++ Model/recommendations.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+
+class Recommendations:
+
+    def __init__(self, model, content_df: pd.DataFrame, train_valid_pairs, train_set_csr, id_to_product, product_to_id, customer_to_id):
+        self.products_data = content_df
+        self.train_valid_pairs = train_valid_pairs
+        self.train_set_csr = train_set_csr
+        self.id_to_product = id_to_product
+        self.product_to_id = product_to_id
+        self.customer_to_id = customer_to_id
+        self.model = model
+
+
+    def id_to_content_df(self, ids: np.array, id_to_item):
+        items = tuple(id_to_item[i] for i in ids)
+
+        result_df = self.products_data[self.products_data.product_id.isin(items)]
+        return result_df
+
+    def cust_history_recommend(self, customer_id, N=10):
+
+        recommended_content, recommended_scores = self.model.recommend(
+                    userid = customer_id,
+                    user_items=self.train_valid_pairs[customer_id][0],
+                    N=N,
+                    filter_already_liked_items=True,
+                    #recalculate_user=True
+        )
+        res = self.id_to_content_df(recommended_content, self.id_to_product)
+
+        return res
+
+    def top_n_recommends(self, order_history, top_n_result):
+
+        hit = 0
+
+        if len(order_history[1]) > 0 and np.intersect1d(order_history[1], top_n_result).size > 0:
+            hit = 1
+
+        return hit
+
+
+    def top_n_recommends_personal(self, order_history, customer_id, N=50):
+        top_n_result, recommended_scores = self.model.recommend(
+                userid=customer_id,
+                user_items=order_history[0],
+                N=N,
+                filter_already_liked_items=False,
+                recalculate_user=True
+        )
+        hit = 0
+        if len(order_history[1]) > 0 and np.intersect1d(order_history[1], top_n_result).size > 0:
+            hit = 1
+
+        return hit
+
+    def random_rec(self, N=10):
+
+        all_content = np.array(list(self.id_to_product.keys())) 
+        top_n_result = np.random.choice(all_content, size=N, replace=True)
+        
+        hits = [self.top_n_recommends(i, top_n_result) for i in self.train_valid_pairs]
+
+        #print('Random hits %.4f from %d' % (sum(hits)/len(hits)*100, len(hits)))
+
+        res = self.id_to_content_df(top_n_result, self.id_to_product)
+
+        return res
+
+    def popularity_rec(self, N=10):
+
+        content_popularity = np.asarray(self.train_set_csr.sum(axis=0)).reshape(-1)
+        top_100_popular_items = np.argsort(-content_popularity)[:100]
+
+        top_n_result = top_100_popular_items[:N]
+
+        hits = [self.top_n_recommends(i, top_n_result) for i in self.train_valid_pairs]
+
+        #print('Popularity hits %.4f from %d' % (sum(hits)/len(hits)*100, len(hits)))
+
+        res = self.id_to_content_df(top_n_result, self.id_to_product)
+
+        return res
+
+    def personal_rec(self, customer_id_str='f374c8c54c'):
+
+        customer_id = self.customer_to_id[customer_id_str]
+
+        hits = [self.top_n_recommends_personal(i, customer_id) for i in self.train_valid_pairs]
+        #print('Personal hits %.4f from %d' % (sum(hits)/len(hits)*100, len(hits)))
+        
+        res = self.cust_history_recommend(customer_id)
+
+        return res
---
+++ Model/vectorization.py
@@ -0,0 +1,69 @@
+from sklearn.model_selection import train_test_split
+from scipy.sparse import csr_matrix
+import pandas as pd
+import numpy as np
+
+class Vectorization:
+
+    def __init__(self,train_data, test_data, content_to_id):
+        self.orders_train = train_data
+        self.orders_test = test_data
+        self.product_to_id = content_to_id
+
+    def get_als_action_history_vector(self, item_to_id, action_history, binary=True) -> np.ndarray:
+        """Получить историю действий для ALS
+
+        :param item_to_id: справочник контента ALS
+        :return:
+        """
+        als_action_history_vector = np.zeros(len(item_to_id), dtype=int)
+
+        for iid in action_history:
+        
+            if iid in item_to_id.keys():
+
+                if binary:
+                    als_action_history_vector[item_to_id[iid]] = 1
+
+        return als_action_history_vector
+
+    def vectorize_action_history(self, action_history):
+        res = self.get_als_action_history_vector(self.product_to_id, action_history)
+        return res
+
+    def df_vectors(self):
+
+        orders_val, orders_test = self.orders_train, self.orders_test
+
+        test_group = orders_test.groupby('customer_id').apply(lambda cust: cust.product_id.unique()).reset_index(name='product_ids')
+        val_group = orders_val.groupby('customer_id').apply(lambda cust: cust.product_id.unique()).reset_index(name='product_ids')
+
+        test_group_slice = test_group.iloc[::]
+        val_group_slice = val_group.iloc[::]
+
+        test_dataset_vectors = { row['customer_id'] : self.vectorize_action_history(row['product_ids']) for i, row in test_group_slice.iterrows() }
+        
+        for key in set(test_dataset_vectors.keys()):
+            if sum(test_dataset_vectors[key]) == 0:
+                del test_dataset_vectors[key]
+
+        ground_truth_dataset_vectors = { row['customer_id'] : self.vectorize_action_history(row['product_ids']) for i, row in val_group_slice.iterrows() if row['customer_id'] in  test_dataset_vectors}
+
+
+        return test_dataset_vectors, ground_truth_dataset_vectors
+
+    def train_valid_union(self):
+        train_valid_pairs = []
+
+        test_dataset_vectors, ground_truth_dataset_vectors = self.df_vectors()
+
+        for customer_id in test_dataset_vectors.keys():
+
+            if customer_id in ground_truth_dataset_vectors.keys():
+                train_valid_pairs.append((
+                csr_matrix(test_dataset_vectors[customer_id]),  # csr матрица на вход ALS
+                ground_truth_dataset_vectors[customer_id].nonzero()[0]
+            ))
+
+            
+        return train_valid_pairs
