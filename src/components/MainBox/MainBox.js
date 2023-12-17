baca9cf31abb9c4b07c74dcc58552a096db963c4
Elena Karataeva <helenminec@gmail.com>
2023-12-18 01:11:12 +0300

    delete useless imports

--- src/components/MainBox/MainBox.js
+++ src/components/MainBox/MainBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import "./MainBox.css"
 import PreLoader from "../ExtendComponents/PreLoader/PreLoader";
 import {Pagination} from "../ExtendComponents/Pagination/Pagination";
