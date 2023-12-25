829e430565c80435084120d4c8173f4a74fc3779
JuliaTsiryulik <93212101+JuliaTsiryulik@users.noreply.github.com>
2023-12-25 15:06:08 +0300

    Add files via upload

--- Model/baseline.py
+++ Model/baseline.py
@@ -25,7 +25,7 @@ class BaseLine:
         order_day_max = self.orders_train['order_day_int'].max()
         order_day_next = order_day_max - 10
         
-        last_10_days_data = self.orders_train.loc[(self.orders_train['order_day_int'] <= order_day_max) & (self.orders_train['order_day_int'] >= order_day_next)]['product_id'].reset_index(name='product_id')
+        last_10_days_data = self.orders_train[(self.orders_train['order_day_int'] <= order_day_max) & (self.orders_train['order_day_int'] >= order_day_next)]['product_id'].reset_index(name='product_id')
         
         data_with_count = last_10_days_data.groupby(['product_id'])['product_id'].count().reset_index(name='product_id_count')
         sort_last_10_days_data = data_with_count.sort_values(['product_id_count'], ascending=False)
@@ -33,31 +33,17 @@ class BaseLine:
         return sort_last_10_days_data
         
     def indexing(self):
-
-        #all_orders_data = pd.concat([self.orders_train, self.orders_test])
-
-
-
+       
+        unique_customers = self.orders_train.customer_id.unique()
+        unique_products = self.orders_train.product_id.unique()
         
-        unique_customers_train = self.orders_train.customer_id.unique()
-        unique_customers_test = self.orders_test.customer_id.unique()
-
-        unique_products_train = self.orders_train.product_id.unique()
-        unique_products_test = self.orders_test.product_id.unique()
-
-        unique_customers = unique_customers_train
-        unique_products = unique_products_train
-        
-        #unique_customers = np.unique(np.concatenate((unique_customers_train, unique_customers_test), axis=None))
-        #unique_products = np.unique(np.concatenate((unique_products_train, unique_products_test), axis=None))
-
         # прямая и обратная индексация контента
         product_to_id = {j: i for i, j in enumerate(unique_products)}
         id_to_product = {j: i for i, j in product_to_id.items()}
 
         # прямая индексация пользователей
         customer_to_id = {j: i for i, j in enumerate(unique_customers)}
-        #print('Индекс создан: %d строк %d столбцов' % (len(customer_to_id), len(product_to_id)))
+
         return product_to_id, id_to_product, customer_to_id
 
     def last_views_top(self):
